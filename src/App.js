f0019fa45564dec3aeca538bab3e6a13e3b8fa08
sahil keshav <sahilKeshav1997@gmail.com>
2021-02-26 19:59:53 +0530

    deploying project to github

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
 
-function App() {
+const url = "https://course-api.com/react-tours-project";
+const App = () => {
+  const [items, setItems] = useState([]); // initial value = empty array
+  const [loading, setLoading] = useState(true); //initial value = true
+
+  const removeTour = (id) => {
+    const toursUpdate = items.filter((item) => item.id !== id);
+    setItems(toursUpdate);
+  };
+
+  const getData = async () => {
+    try {
+      //make request call to api
+
+      const response = await fetch(url);
+      const data = await response.json();
+      setLoading(false);
+      setItems(data);
+
+      // console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+    // console.log(data);
+  };
+
+  useEffect(() => {
+    //runs only once on the initial render
+    getData();
+  }, []);
+
+  if (loading) {
+    // if true show loading component
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+
+  if (items.length === 0) {
+    return (
+      <main>
+        <div className="title">
+          <h2>tours unavailable</h2>
+          <button className="btn" onClick={getData}>
+            view tours
+          </button>
+        </div>
+      </main>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    //once we get the data set loading to false and show the data
+    //we can also pass a function as a prop
+    <main>
+      <Tours items={items} removeTour={removeTour} />
+    </main>
   );
-}
+};
 
 export default App;
---
+++ src/Tours.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Tour from "./Tour";
+
+const Tours = ({ items, removeTour }) => {
+  //access to array and function
+  //   console.log(items);
+  //   console.log(removeTour);
+  return (
+    <section>
+      <div className="title">
+        <h2>tours and travels</h2>
+        <div className="under-line"></div>
+      </div>
+      <div>
+        {items.map((item) => {
+          return <Tour key={item.id} {...item} removeTour={removeTour} />; //prop drilling//...item contains object
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default Tours;
